c4d7cc80a1714b501f0e711cbb6a8d68e54a564e
webstormrage <webstormrage@gmail.com>
2020-10-04 16:57:22 +0300

    Fix buffed player

--- src/unfair-warcraft-main.lua
+++ src/unfair-warcraft-main.lua
@@ -1,5 +1,5 @@
 function unfairWarcraftMain()
-    BUFFED_PLAYER = Player(0)
+    BUFFED_PLAYER = Player(1)
     ADMIN_PLAYER = Player(0)
     AI_PLAYER = Player(23)
     SetPlayerAllianceStateAllyBJ(BUFFED_PLAYER, AI_PLAYER, true)
